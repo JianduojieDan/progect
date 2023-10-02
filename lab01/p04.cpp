ad01e6bce97178ec94cf95c7b3c59af8f133f47a
jianduojie <dj12241@auca.kg>
2023-10-02 11:35:56 +0600

    lab 01 p04 finished

---
+++ lab01/p04.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+int main (){
+
+    using namespace std ;
+
+ cout << " Enter the first integer : \n"  ;
+ int num1 ;
+ cin >> num1 ;
+
+ cout << " Enter the second integer \n" ;
+ int num2 ;
+ cin >> num2 ;
+
+ cout << num1 << " + " << num2 << " = " << num1 + num2 << endl ;
+ cout << num1 << " + " << num2 << " - " << num1 + num2 << endl ;
+ cout << num1 << " * " << num2 << " * " << num1 * num2 << endl ;
+ cout << num1 << " / " << num2 << " / " << num1 / num2 << endl ;
+ cout << num1 << " % " << num2 << " % " << num1 % num2 << endl ;
+
+
+ 
+
+
+    return 0;
+}
