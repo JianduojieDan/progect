2e04e58c8352993211564bc67fe394aeea56ff18
jianduojie <dj12241@auca.kg>
2023-10-02 11:36:35 +0600

    lab 01 p07 finished

---
+++ lab01/p07.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+
+int main() {
+
+    using namespace std;
+
+    int odd1 = 1;
+    int odd2 = 3;
+    int odd3 = 5;
+    int odd4 = 7;
+    int odd5 = 9;
+    int odd6 = 11;
+    int odd7 = 13;
+    int odd8 = 15;
+    int odd9 = 17;
+    int odd10 = 19;
+
+    int sum = odd1 + odd2 + odd3 + odd4 + odd5 + odd6 + odd7 +odd8 + odd9 + odd10;
+
+    cout << odd1 << " + " << odd2 << " + " << odd3 << " + " << odd4 << " + " << odd5 << " + "
+         << odd6 << " + " << odd7 << " + " << odd8 << " + " << odd9 << " + " << odd10
+         << " = " << sum << endl;
+
+    return 0;
+}
+
+// Created by Administrator on 2023/10/2.
+//
