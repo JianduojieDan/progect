b1447fc2b82578ac8e2238c599043b22ec8df82c
jianduojie <dj12241@auca.kg>
2023-10-02 11:36:13 +0600

    lab 01 p06 finished

---
+++ lab01/p06.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    std::cout << "     *        *********       " << endl;
+    std::cout << "    ***        *******      " << endl;
+    std::cout << "   *****        *****     " << endl;
+    std::cout << "  *******        ***    " << endl;
+    std::cout << " *********        *   " << endl;
+
+    return 0;
+}
+
+
+// Created by Administrator on 2023/9/27.
+//
