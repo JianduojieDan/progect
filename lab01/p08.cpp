cc72058ad1b4c4f93bde39a72d9388d97472c347
jianduojie <dj12241@auca.kg>
2023-10-02 11:36:43 +0600

    lab 01 p08 finished

---
+++ lab01/p08.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<cmath>
+
+using namespace::std;
+
+int main(){
+    double pi1 = sqrt( 6 * (1 + 1/4 + 1/9 + 1/16 + 1/25));
+
+
+    double pi2 = sqrt(6*(1 + 1/4 + 1/9 + 1/16 + 1/25 + 1/36));
+
+
+
+    cout << " approximate the value of pi" << pi1 << endl;
+
+    cout << " approximate the value of pi" << pi2 << endl;
+
+
+
+            return 0;
+
+
+}
+
+// Created by Administrator on 2023/10/2.
+//
