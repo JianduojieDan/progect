8558ffca7a9254092f025015fe6f6ac7969bbe22
jianduojie <dj12241@auca.kg>
2023-10-04 10:35:06 +0600

    p1001 accepted

--- ojmid/p1001.cpp
+++ ojmid/p1001.cpp
@@ -2,10 +2,11 @@
 
         using namespace std;
 
-     int main() {
-
-
-     }
+int main() {
+    int A, B;
+    cin >> A >> B;
+    cout << "X = " << A + B << endl;
+}
         //
 // Created by Administrator on 2023/10/4.
 //
