e4b655a5ae233b8d1fd9f03861a9e9a7c8a91c4e
jianduojie <dj12241@auca.kg>
2023-10-16 16:34:56 +0600

    p1007 accepted

---
+++ ojmid/p1007.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+
+using namespace std;
+int main () {
+    int a,b,c,d;
+    cin >> a >> b >> c >> d;
+    float DIFERENCA  = (a * b)- (c * d);
+
+    cout << "DIFERENCA = " << DIFERENCA  << endl;
+
+    return 0;
+
+}
