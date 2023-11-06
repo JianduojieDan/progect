aa496a2a7ff93e78f2a9291dcaf89bdd3fb004af
jianduojie <dj12241@auca.kg>
2023-11-06 22:09:43 +0600

    lab04 p03 finished

---
+++ lab04/p03.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+
+#include<string>
+
+using namespace std;
+int main() {
+    int month;
+    cout << "Enter the month number (1-12): ";
+    cin >> month;
+    string msg;
+
+    switch (month) {
+        case 1:
+            msg = "January";
+            break;
+        case 2:
+            msg = "February";
+            break;
+        case 3:
+            msg = "March";
+            break;
+        case 4:
+            msg = "April";
+            break;
+        case 5:
+            msg = "May";
+            break;
+        case 6:
+            msg = "June";
+            break;
+        case 7:
+            msg = "July";
+            break;
+        case 8:
+            msg = "August";
+            break;
+        case 9:
+            msg = "September";
+            break;
+        case 10:
+            msg = "October";
+            break;
+        case 11:
+            msg = "November";
+            break;
+        case 12:
+            msg = "December";
+            break;
+        default:
+            msg = "Incorrect month number.";
+            break;
+
+    }
+    cout << msg << endl;
+
+}
