0f75afc34ff62d4d021a4d706d3af2a7223e1ceb
jianduojie <dj12241@auca.kg>
2023-12-04 19:50:13 +0600

    p1041 accepted

---
+++ ojmid/p1041.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+int main (){
+    using namespace std;
+    float x,y;
+    cin >> x >> y;
+    if (x == 0 && y == 0){
+        cout << "Origem" << endl;
+    }else if (y == 0){
+        cout << "Eixo X" << endl;
+    }else if (x == 0){
+        cout << "Eixo Y" << endl;
+    }else if (x > 0 && y > 0){
+        cout << "Q1\n";
+    }else if (x > 0 && y < 0){
+        cout << "Q4\n";
+    }else if(x < 0 && y < 0){
+        cout << "Q3\n";
+    }else{
+        cout << "Q2\n";
+    }
+    return 0;
+}
