38f0c86d5acc2fb1e64d944d13d5226122df8be0
jianduojie <dj12241@auca.kg>
2023-12-04 19:48:30 +0600

    lab05-p02 finished

--- lab05/p02.cpp
+++ lab05/p02.cpp
@@ -1,3 +1,15 @@
-//
-// Created by Administrator on 2023/11/13.
-//
+#include<iostream>
+using namespace std;
+int main(){
+    int num;
+    int sum = 0;
+    cout << "Enter an integer: " << endl;
+    cin >> num;
+    while(num != 0){
+       int digit = num % 10;
+       sum += digit;
+       num /= 10;
+    }
+    cout << "The sum of all digits is " << sum << "." << endl;
+    return 0;
+}
