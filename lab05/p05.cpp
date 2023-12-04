80da8c2a97c9294d2dab6c4d109c609b4ea58d17
jianduojie <dj12241@auca.kg>
2023-12-04 19:48:52 +0600

    lab05-p05 finished

--- lab05/p05.cpp
+++ lab05/p05.cpp
@@ -1,3 +1,37 @@
-//
-// Created by Administrator on 2023/11/13.
-//
+#include "raylib.h"
+
+int main(){
+    const int screenWidth = 800;
+    const int screenHeight = 450;
+    const int circlesNumber = 30;
+    unsigned char a = 0;
+    unsigned char r = 255;
+    unsigned char g = 0;
+    unsigned char b = 0;
+    float radius = 300;
+
+    InitWindow(screenWidth, screenHeight, "Gradient");
+
+    SetTargetFPS(1);
+    BeginDrawing();
+    ClearBackground(BLACK);
+
+    for (int i = 0; circlesNumber > i; ++i)
+    {
+        float X = static_cast<float>(screenWidth/2);
+        float Y = static_cast<float>(screenHeight/2);
+        Vector2 CirclePosition = {X , Y};
+        a += 5;
+        Color circleColor = {r, g, b, a};
+        radius -= 20;
+        DrawCircleV(CirclePosition, radius,circleColor);
+    }
+    EndDrawing();
+    while(!WindowShouldClose())
+    {
+        PollInputEvents();
+    }
+    CloseWindow();
+
+    return 0;
+}
