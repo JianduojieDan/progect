def9053b64e4916d52ddaac7762a55fd5530e149
jianduojie <dj12241@auca.kg>
2023-12-04 19:51:27 +0600

    p1052 accepted

---
+++ ojmid/p1052.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    using namespace std;
+
+    int monthNumber;
+    cin >> monthNumber;
+
+    string month;
+
+    switch (monthNumber) {
+        case 1:
+            month = "January";
+            break;
+        case 2:
+            month = "February";
+            break;
+        case 3:
+            month = "March";
+            break;
+        case 4:
+            month = "April";
+            break;
+        case 5:
+            month = "May";
+            break;
+        case 6:
+            month = "June";
+            break;
+        case 7:
+            month = "July";
+            break;
+        case 8:
+            month = "August";
+            break;
+        case 9:
+            month = "September";
+            break;
+        case 10:
+            month = "October";
+            break;
+        case 11:
+            month = "November";
+            break;
+        case 12:
+            month = "December";
+            break;
+        default:
+            cout << "Invalid month number" << endl;
+            break;
+    }
+
+    cout << month << endl;
+
+    return 0;
+}
