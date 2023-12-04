83abc71bb26633871f87bcc6a597b54cb6107d73
jianduojie <dj12241@auca.kg>
2023-12-04 19:47:56 +0600

    CMakeList update

--- lab04/p01.cpp
+++ lab04/p01.cpp
@@ -2,7 +2,7 @@
 #include<cmath>
 using namespace std;
 int main () {
-    double result = 0.0;
+    double result = 2.0;
     result = 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1;
     double one = 1.0;
      if (abs(result - one) < 0.0000000001) {
