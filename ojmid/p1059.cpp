8e50cfdd9a3a54da045fd77753affa37b9ad7748
jianduojie <dj12241@auca.kg>
2023-12-11 20:24:02 +0600

    p1059 accepted

---
+++ ojmid/p1059.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+
+int main() {
+    using namespace std;
+    int num = 0;
+
+    while(num != 100) {
+        num += 2;
+        cout << num << endl;
+    }
+
+    return 0;
+}
