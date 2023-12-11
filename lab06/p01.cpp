eb57c076f8f9a9575b3fffd8810da482340395d4
jianduojie <dj12241@auca.kg>
2023-12-11 19:02:12 +0600

    lab06-p1 finished

---
+++ lab06/p01.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+void swap(int &a, int &b) {
+    int temp=a;
+    a=b;
+    b=temp;
+
+}
+int main() {
+    int a;
+    int b;
+
+    std::cout << "Enter the first value: ";
+    std::cin >> a;
+
+    std::cout << "Enter the second value: ";
+    std::cin >> b;
+
+    std::cout << "Before swapping: a = " << a << "; b = " << b << "; " << std::endl;
+    swap(a,b);
+
+    std::cout << "After swapping: a = " << a << "; b = " << b << "; " << std::endl;
+
+    return 0;
+}
