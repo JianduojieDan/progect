f049aef92670b4aba4903c65ee1384a82c9a790c
jianduojie <dj12241@auca.kg>
2023-12-18 21:04:22 +0600

    p1178 accepted

---
+++ ojmid/p1178.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    double n;
+    cin >> n;
+    printf("N[%d] = %.4f\n", 0, n);
+    for(int i = 1; i < 100; i++){
+        n = n/2.0;
+        printf("N[%d] = %.4f\n", i, n);
+    }
+    return 0;
+}
