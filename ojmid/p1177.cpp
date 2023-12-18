3b43745956f936a30ef1a166a79a46fb35418722
jianduojie <dj12241@auca.kg>
2023-12-18 21:04:15 +0600

    p1177 accepted

---
+++ ojmid/p1177.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main(){
+    using namespace std;
+    int t, a;
+    int N[1000];
+    cin >> t;
+    a = 0;
+    for(int i = 0; i < 1000; i++){
+        cout << "N[" << i << "] = " << a << "\n";
+        a++;
+        if(a == t) a = 0;
+    }
+    return 0;
+}
