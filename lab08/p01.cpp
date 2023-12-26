f1a76fe35eec4dc84f2e816de5035f455507f16f
jianduojie <dj12241@auca.kg>
2023-12-26 14:36:22 +0600

    lab07-p01 finished

---
+++ lab08/p01.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+const int COLOUM_SIZE = 4;
+
+int sum (const int a[][COLOUM_SIZE] , int rowSize){
+    int total = 0;
+    for (int row = 0 ; row < rowSize ;row++ ){
+        for (int coloum = 0 ; coloum < COLOUM_SIZE ; coloum++){
+            total += a[row][coloum];
+        }
+    }
+    return total;
+}
+
+int main(){
+    const int ROW_SIZE = 3;
+    int m[ROW_SIZE][COLOUM_SIZE];
+    cout << "Enter " << ROW_SIZE << " rows and " << COLOUM_SIZE << " columns: " << endl;
+    for(int i = 0; i < ROW_SIZE;i++){
+        for(int j = 0; j < COLOUM_SIZE ; j++){
+            cin >> m[i][j];
+        }
+    }
+    cout << "\nSum of all elements is " << sum(m, ROW_SIZE) << endl;
+    return 0;
+}
